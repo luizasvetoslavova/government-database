71b525b019501c7bb81c29f87dd4c63c49a23442
Angel <a4k0o@protonmail.com>
2022-03-20 04:06:36 +0200

    implement id reader

--- src/citizenData/IdReaderImpl.java
+++ src/citizenData/IdReaderImpl.java
@@ -1,18 +1,24 @@
 package citizenData;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class IdReaderImpl implements IdReader {
     @Override
     public Date getBirthDate(long id) {
-        //(id / 10000) - 40     -from 1999
-        //(id / 10000) - 20     -before 1900
-        //(id / 10000) - 40     -from 1900 to 1999
-        return null;
-    }
+        Date date = null;
 
-    public boolean isBetween(int x, int lower, int upper) {
-        return lower <= x && x <= upper;
+        String year = String.valueOf(getYear(id));
+        String month = String.valueOf(getMonthSequence(getMonth(id)));
+        String day = String.valueOf(getDay(id));
+
+        try {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(day + "/" + month + "/" + year);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
     }
 
     @Override
@@ -82,6 +88,36 @@ public class IdReaderImpl implements IdReader {
 
     @Override
     public Gender getGender(long id) {
-        return null;
+
+        if (((id / 10) / 1000) % 2 == 0) {
+            return Gender.FEMALE;
+        }
+        return Gender.MALE;
+    }
+
+    public long getMonthSequence(long monthIndex) {
+        if (monthIndex >= 41) {
+            monthIndex -= 40;
+        } else if (monthIndex >= 21 && monthIndex <= 32) {
+            monthIndex -= 20;
+        }
+
+        return monthIndex;
+    }
+
+    public long getMonth(long id) {
+        return (id / 1000000) % 100;
+    }
+
+    public long getDay(long id) {
+        return (id / 10000) % 100;
+    }
+
+    public long getYear(long id) {
+        return id / 100000000;
+    }
+
+    public boolean isBetween(int x, int lower, int upper) {
+        return lower <= x && x <= upper;
     }
 }
