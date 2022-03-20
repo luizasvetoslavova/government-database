8739b3b23eb2f99428941b8f41398b7e63ab3fc9
luizasvetoslavova <luizasvetoslavova16@gmail.com>
2022-03-20 02:55:38 +0200

    implement commmunication

--- src/userCommunication/Communication.java
+++ src/userCommunication/Communication.java
@@ -1,4 +1,32 @@
 package userCommunication;
 
+import citizenData.*;
+
+import java.util.Date;
+
 public interface Communication {
+
+    String askForEmail();
+
+    String askForPassword();
+
+    Date askForDate();
+
+    PunishmentType askForPunishmentType();
+
+    PossessionType askForPossessionType();
+
+    double askForAmountOfMoney();
+
+    double askForPercentage();
+
+    Country askForCountry();
+
+    void show(String text);
+
+    void showAdminOptions();
+
+    void showUserOptions();
+
+    void showOrganisationOptions();
 }
