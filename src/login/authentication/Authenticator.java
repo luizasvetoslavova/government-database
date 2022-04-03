0c7bde6577674f1ab1cd3ef5cacef5a1a9868203
luizasvetoslavova <luizasvetoslavova16@gmail.com>
2022-04-03 18:48:27 +0300

    delete file

--- src/login/authentication/Authenticator.java
+++ src/login/authentication/Authenticator.java
@@ -7,17 +7,6 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Authenticator implements Authentication {
-<<<<<<< HEAD
-    @Override
-    public boolean emailExists() {
-        return false;
-    }
-
-    @Override
-    public boolean isPasswordCorrect() {
-        return false;
-    }
-=======
 
     private final FileExtractor fileExtractor;
     private final Communicator communicator;
@@ -64,7 +53,7 @@ public class Authenticator implements Authentication {
         List<String> emailsAndPasswords = List.of(fileExtractor.extractWholeData(path).split("\r\n"));
         Set<String> emails = new HashSet<>();
 
-        for (int i = 1; i < emailsAndPasswords.size(); i++) {
+        for (int i = 0; i < emailsAndPasswords.size(); i++) {
             emails.add(emailsAndPasswords.get(i).split(",")[1]);
         }
         return emails.contains(email);
@@ -73,7 +62,7 @@ public class Authenticator implements Authentication {
     private boolean isPasswordCorrect(String email, String password) {
         List<String> emailsAndPasswords = List.of(fileExtractor.extractWholeData(admins).split("\r\n"));
 
-        for (int i = 1; i < emailsAndPasswords.size(); i++) {
+        for (int i = 0; i < emailsAndPasswords.size(); i++) {
             if(emailsAndPasswords.get(i).split(",")[0].equals(email) &&
                     emailsAndPasswords.get(i).split(",")[1].equals(password)) {
                 return true;
@@ -93,5 +82,4 @@ public class Authenticator implements Authentication {
     public Path getUsers() {
         return users;
     }
->>>>>>> login
 }
